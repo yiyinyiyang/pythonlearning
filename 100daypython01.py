f0a11810e7955267728a6c4b25316e7a1ef36e12
yiyinyiyang <fangsheng@stu.ahmu.edu.cn>
2020-10-21 23:02:46 +0800

    添加100daypython和定义noob图案

---
+++ 100daypython01.py
@@ -0,0 +1,10 @@
+
+def sum_numbers():
+    n=0
+    for i in range(1,11):
+        n += i
+    return n
+
+
+a =sum_numbers()        
+print(a)                                
---
+++ def.py
@@ -0,0 +1,12 @@
+def shownoob():
+    print("N    N     O      O     B")
+    print("NN   N   O   O  O   O   B")
+    print("N  N N   O   O  O   O   B B B")
+    print("N   NN   O   O  O   O   B   B")
+    print("N    N     O      O     B B B")
+
+
+
+
+
+shownoob()
